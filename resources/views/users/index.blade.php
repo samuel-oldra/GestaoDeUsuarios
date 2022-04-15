46390f3b55eac69e4ead40f276fdb5e4272f9a47
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-04-15 00:00:00 -0300

    Listagem do usuário

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,8 @@
     @foreach($users as $user)
         <li>
             {{ $user->name }} -
-            {{ $user->email }}
+            {{ $user->email }} |
+            <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
         </li>
     @endforeach
 </ul>
