0941ee18cef089ef667a715b92b3f00967f60be5
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-04-16 00:00:00 -0300

    Templates blade

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+</head>
+<body>
+<div class="app">
+    @yield('content')
+</div>
+</body>
+</html>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,6 +1,12 @@
-<h1>Listagem do usuário {{ $user->name }}</h1>
+@extends('layouts.app')
 
-<ul>
-    <li>{{ $user->name }}</li>
-    <li>{{ $user->email }}</li>
-</ul>
+@section('title', 'Listagem do usuário')
+
+@section('content')
+    <h1>Listagem do usuário {{ $user->name }}</h1>
+
+    <ul>
+        <li>{{ $user->name }}</li>
+        <li>{{ $user->email }}</li>
+    </ul>
+@endsection
