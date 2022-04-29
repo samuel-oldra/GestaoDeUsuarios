9e4c05ff5e167def3b4fe3926a717b7487e7c932
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-04-29 00:00:00 -0300

    CMD: php artisan migrate

--- database/migrations/2022_05_06_115326_create_comments_table.php
+++ database/migrations/2022_05_06_115326_create_comments_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            //$table->foreignId('user_id')->constrained('users', 'id');
+            $table->foreignId('user_id');
+            $table->text('body');
+            $table->boolean('visible')->default(true);
             $table->timestamps();
         });
     }
