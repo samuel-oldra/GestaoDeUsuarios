bde3ccc5e4e5b447cc09c7c80ef06f4f1fdf92fc
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-02 00:00:00 -0300

    Listagem de comentários do usuário

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -31,6 +31,9 @@
             <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                 Detalhes
             </th>
+            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                Comentários
+            </th>
         </tr>
         </thead>
         <tbody>
@@ -50,6 +53,11 @@
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                     <a href="{{ route('users.show', $user->id) }}" class="bg-orange-200 rounded-full py-2 px-6">
                         Detalhes
+                    </a>
+                </td>
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <a href="{{ route('comments.index', $user->id) }}" class="bg-blue-200 rounded-full py-2 px-6">
+                        Anotações (0)
                     </a>
                 </td>
             </tr>
