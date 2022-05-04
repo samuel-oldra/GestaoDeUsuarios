0015b570d9bbb31e2d2ad326441e52c76c7cee85
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-04 09:58:23 -0300

    Inserindo registro do usuário

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
 Route::get('/', function () {
     return view('welcome');
