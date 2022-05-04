a84e758f3b3dd4fad4437fd3b4f74d9cabc59612
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-04 15:10:52 -0300

    Usando Tailwind CSS no CRUD de usuários

--- resources/views/includes/validations-form.blade.php
+++ resources/views/includes/validations-form.blade.php
@@ -1,7 +1,7 @@
 @if($errors->any())
-<ul class="errors">
-    @foreach($errors->all() as $error)
-    <li class="error">{{ $error }}</li>
-    @endforeach
-</ul>
+    <ul class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4">
+        @foreach($errors->all() as $error)
+            <li class="error">{{ $error }}</li>
+        @endforeach
+    </ul>
 @endif
