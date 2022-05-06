384926d5c96a1b27238f687820742839c0ac0929
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-06 10:14:25 -0300

    Inserindo registro de comentário do usuário

--- resources/views/users/comments/index.blade.php
+++ resources/views/users/comments/index.blade.php
@@ -5,7 +5,10 @@
 @section('content')
     <h1 class="text-2xl font-semibold leading-tigh py-2">
         Comentários do Usuário {{$user->name}}
-        <a href="{{ route('users.create') }}" class="bg-blue-900 rounded-full text-white px-4 text-sm">+</a>
+        <a href="{{ route('comments.create', $user->id) }}"
+           class="bg-blue-900 rounded-full text-white px-4 text-sm">
+            +
+        </a>
     </h1>
 
     <form action="{{ route('users.index') }}" method="get" class="py-5">
@@ -25,12 +28,9 @@
             <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
                 Visível
             </th>
-            {{--<th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">--}}
-            {{--Editar--}}
-            {{--</th>--}}
-            {{--<th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">--}}
-            {{--Detalhes--}}
-            {{--</th>--}}
+            <th class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-700 uppercase tracking-wider">
+                Editar
+            </th>
         </tr>
         </thead>
         <tbody>
@@ -40,18 +40,13 @@
                     {{ $comment->body }}
                 </td>
                 <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                    {{ $comment->visible }}
+                    {{ $comment->visible ? 'SIM' : 'NÃO' }}
                 </td>
-                {{--<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">--}}
-                {{--<a href="{{ route('users.edit', $user->id) }}" class="bg-green-200 rounded-full py-2 px-6">--}}
-                {{--Editar--}}
-                {{--</a>--}}
-                {{--</td>--}}
-                {{--<td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">--}}
-                {{--<a href="{{ route('users.show', $user->id) }}" class="bg-orange-200 rounded-full py-2 px-6">--}}
-                {{--Detalhes--}}
-                {{--</a>--}}
-                {{--</td>--}}
+                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <a href="{{ route('users.edit', $user->id) }}" class="bg-green-200 rounded-full py-2 px-6">
+                        Editar
+                    </a>
+                </td>
             </tr>
         @endforeach
         </tbody>
