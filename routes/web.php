eca847abe2eeafc57f91a58cae4fe78c663edd5c
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-09 15:40:50 -0300

    Editando registro de comentário do usuário

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
 Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+Route::get('/users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
 Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
