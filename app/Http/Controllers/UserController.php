f2706cb4417daa6ee277bbe214af78099ac3873f
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-10 19:00:00 -0300

    Criadas rotas, views e controllers para usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('users.index');
+    }
+
+    public function show($id)
+    {
+        dd('users.show', $id);
+    }
 }
