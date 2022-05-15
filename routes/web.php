f6aeb046452891947911e10cbffddd21c52b368d
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-15 00:00:00 -0300

    CMD: php artisan breeze:install

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,24 @@
 <?php
 
-use App\Http\Controllers\{
-    Admin\CommentController, UserController
-};
 use Illuminate\Support\Facades\Route;
 
-Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
-Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
-Route::get('/users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
-Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
-
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
