d7d34659c5a6e9329a07b8ceb7d29cd39aec74ac
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-18 19:00:00 -0300

    Inserindo registro do usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -18,16 +19,28 @@ class UserController extends Controller
         return view('users.create');
     }
 
-    public function store()
-    {
-        dd('users.store');
-    }
-
     public function show($id)
     {
         if (!$user = User::find($id))
             return redirect()->route('users.index');
 
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        // $user = new User;
+        // $user->name = $request->name;
+        // $user->email = $request->email;
+        // $user->password = $request->password;
+        // $user->save();
+
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+
+        //return redirect()->route('users.show', $user->id);
+        return redirect()->route('users.index');
     }
 }
