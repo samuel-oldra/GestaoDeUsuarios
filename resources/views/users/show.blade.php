0296c04929b3a3212da8c0953d636ff6df55ba2f
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-05-25 19:00:00 -0300

    Usando Tailwind CSS no CRUD de usuários

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.app')
 
-@section('title', 'Listagem do Usuário { $user->name }')
+@section('title', "Listagem do Usuário { $user->name }")
 
 @section('content')
-    <h1>Listagem do Usuário {{ $user->name }}</h1>
+    <h1 class="text-2xl font-semibold leading-tigh py-2">Listagem do Usuário {{ $user->name }}</h1>
 
     <ul>
         <li>{{ $user->name }}</li>
         <li>{{ $user->email }}</li>
     </ul>
+
+    <form action="{{ route('users.destroy', $user->id) }}" method="POST" class="py-12">
+        @method('DELETE')
+        @csrf
+        <button type="submit" class="rounded-full bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4">
+            Deletar
+        </button>
+    </form>
 @endsection
