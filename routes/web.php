26fdca365eb0e270effd0fcf63136e0904fe846f
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-06-02 19:00:00 -0300

    Inserindo registro de comentário do usuário

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\{
 use Illuminate\Support\Facades\Route;
 
 Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
+Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
