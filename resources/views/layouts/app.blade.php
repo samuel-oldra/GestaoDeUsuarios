e9c2a05094c880514ff1942c5bbb5f1bc4f509df
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-06-27 16:02:47 -0300

    Adicionado menu e incorporado botão de 'Logout'

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,15 +8,28 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50">
+<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-3">
+    <div class="flex items-center flex-shrink-0 text-white mr-6">
+        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54"
+             xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/>
+        </svg>
+        <span class="font-semibold text-xl tracking-tight">Gestão de Usuários</span>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow"></div>
+        <div>
+            <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button type="submit"
+                        class="inline-block text-sm px-4 py-2 leading-none text-white bg-red-500 border rounded border-white mt-4 lg:mt-0">
+                    Logout
+                </button>
+            </form>
+        </div>
+    </div>
+</nav>
 <div class="container mx-auto px-4 py-8">
-    <form action="{{ route('logout') }}" method="post">
-        @csrf
-        <button type="submit"
-                class="shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded">
-            Logout
-        </button>
-    </form>
-
     @yield('content')
 </div>
 </body>
